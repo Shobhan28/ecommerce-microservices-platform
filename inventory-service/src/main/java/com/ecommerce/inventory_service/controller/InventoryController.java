13755543b8cc5c30e0f580a3689721513b60bf19
shobhan28 <shobhan28m@gmail.com>
2026-03-20 13:13:42 +0530

    implemented API gateway

---
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/controller/InventoryController.java
@@ -0,0 +1,16 @@
+package com.ecommerce.inventory_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inventories")
+public class InventoryController {
+
+
+    @GetMapping
+    public String getInventory(){
+        return "inventory service working";
+    }
+}
