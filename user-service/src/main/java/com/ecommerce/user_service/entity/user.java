df2b5f41b4d696fbcb0c2d7a90ac2552a7ada277
shobhan28 <shobhan28m@gmail.com>
2026-03-20 21:53:36 +0530

    implemented User Model and JwtUtil class

---
+++ user-service/src/main/java/com/ecommerce/user_service/entity/user.java
@@ -0,0 +1,15 @@
+package com.ecommerce.user_service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class user {
+
+    private String username;
+    private String password;
+
+}
